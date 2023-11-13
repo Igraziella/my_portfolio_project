cfa429c17efcaf68ba1583b955283ca05b0801c0
igraziella <igraziella2@gmail.com>
2023-11-13 21:57:57 +0100

    added data

---
+++ my-portfolio/src/data.jsx
@@ -0,0 +1,48 @@
+ export const projects = [
+    {
+        title: "Diabetics Recipe App",
+        subtitle: "HTML, CSS & Python Flask",
+        description: "Recipe App for creating local meal plans tailored to cater to diabetics in Nigeria, sharing of meal ideas and customized menus between users",
+        image: "./vite.svg",
+        link: "https://diabeticsmealcompanion.com",
+    },
+    {
+        title: "Me2Mentor",
+        subtitle: "React, Tailwind & Python",
+        description: "A mentorship site for new graduates and professionals seeking to switch careers. An ALX team collaboration project",
+        image: "./me2mentor.PNG",
+        link: "https://me2mentor.com",
+    },
+    {
+        title: "PowerUp",
+        subtitle: "HTML & CSS",
+        description: "A collaboration project for Tech4Dev's Women Techsters bootcamp, using HTML and CSS to build a website that provides solar power for African businesses with instalmental payment plans",
+        image: "./powerup.PNG",
+        link: "https://wtboostersdev.github.io/PowerUp/",
+    },
+    {
+        title: "QR card component",
+        subtitle: "HTML & CSS",
+        description: "A simple QR card component challenge from Frontend Mentor to test knowledge of basic CSS and HTML. vjhhhhhhduhudhhdgfgf hhgdyfgbfn jjgdhfhudbhdju",
+        image: "./qrcapture.PNG",
+        link: "https://igraziella.github.io/Qrcard_component_challenge/",
+    },
+ ]
+
+export const skills = [
+    {
+        1: "HTML",
+    },
+    {
+        2: "Tailwind CSS",
+    },
+    {
+        3: "Javascript",
+    },
+    {
+        4: "React",
+    },
+    {
+        5: "Responsive Design",
+    },
+]
