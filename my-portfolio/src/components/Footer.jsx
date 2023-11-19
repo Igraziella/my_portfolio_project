2cf3092bdcf24a6e9bf637e2520ff984f2ebf60c
igraziella <igraziella2@gmail.com>
2023-11-19 22:47:00 +0100

    added the footer component

--- my-portfolio/src/components/Footer.jsx
+++ my-portfolio/src/components/Footer.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faTwitter, faLinkedin, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
     return (
-        <footer className="container bg-gray-400 p-11 text-black">
-            <FontAwesomeIcon icon={faEnvelope} />
-            <div>
+        <footer className="container bg-gray-400 p-7 px-10 text-black">
+            <div className="inline-flex gap-6 text-3xl">
+                <FontAwesomeIcon icon={faGithub} />
+                <FontAwesomeIcon icon={faTwitter} />
+                <FontAwesomeIcon icon={faLinkedin} />
+                <FontAwesomeIcon icon={faInstagram} />
+            </div> 
+            <div className="flex justify-end font-semibold text-base text-center">
                 &copy;2023 iGraziella. All Rights Reserved
             </div>
         </footer>
