6d61fedaf588873f6e7feb36a332845b94bc59cc
igraziella <igraziella2@gmail.com>
2023-11-19 23:26:07 +0100

    added hover effect to footer icons

--- my-portfolio/src/components/Footer.jsx
+++ my-portfolio/src/components/Footer.jsx
@@ -6,10 +6,18 @@ export default function Footer() {
     return (
         <footer className="container bg-gray-400 p-7 px-10 text-black">
             <div className="inline-flex gap-6 text-3xl">
-                <FontAwesomeIcon icon={faGithub} />
-                <FontAwesomeIcon icon={faTwitter} />
-                <FontAwesomeIcon icon={faLinkedin} />
-                <FontAwesomeIcon icon={faInstagram} />
+                <a href="https://github.com/Igraziella">
+                    <FontAwesomeIcon icon={faGithub} className="hover:text-yellow-800"/>
+                </a>
+                <a href="https://twitter.com/ObialorGraziel2">
+                    <FontAwesomeIcon icon={faTwitter} className="hover:text-yellow-800"/>
+                </a>
+                <a href="https://www.linkedin.com/in/ijeoma-obialor-012494114/">
+                    <FontAwesomeIcon icon={faLinkedin} className="hover:text-yellow-800"/>
+                </a>
+                <a href="https://www.instagram.com/">
+                    <FontAwesomeIcon icon={faInstagram} className="hover:text-yellow-800"/>
+                </a>
             </div> 
             <div className="flex justify-end font-semibold text-base text-center">
                 &copy;2023 iGraziella. All Rights Reserved
