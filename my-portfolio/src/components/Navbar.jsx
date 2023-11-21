58966669afe348d011ecdd403756d466aa6795ce
igraziella <igraziella2@gmail.com>
2023-11-21 06:39:37 +0100

    made minor changes to Nabar

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -3,13 +3,14 @@ import React from "react";
 export default function Navbar() {
     return (
         <header className="bg-gray-800 text-gray-200 md:sticky top-0 z-10">
+            
             <div className="container mx-auto flex flex-wrap p-0 flex-col md:flex-row items-center">
                 <a className="title-font font-bold mb-4 ml-5 md:mb-0">
                     <a href="about" className="text-lg">
                         Ijeoma Obialor
                     </a>
                 </a> 
-                <nav className="font-bold md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-gray-700 flex flex-wrap items-center justify-center">
+                <nav className="font-semibold md:mr-auto md:ml-auto md:py-1 md:pl-4 md:border-gray-700 flex flex-wrap items-center justify-center text-base">
                     <a href="#projects" className="mr-5 hover:text-yellow-600">
                         Projects
                     </a>
